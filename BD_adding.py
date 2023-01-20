449af5f8127bb0cff8944bf55220a4b4cebddc2d
Igor <82223725+Igggor@users.noreply.github.com>
2023-01-20 22:03:40 +0300

    Add files via upload

---
+++ BD_adding.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def add_link_to_bd(link = "", type = "None", info = "None", file = "links.db",  data_base = "links"):
+
+    con = sqlite3.connect(file) #Создание или открытие бд
+    cur = con.cursor()
+    cur.execute(
+        f"""CREATE TABLE IF NOT EXISTS {data_base} (
+                link TEXT, 
+                type TEXT,
+                info TEXT
+            )"""
+    )
+    con.commit()
+
+    cur.execute(
+        f"SELECT count(*) FROM {data_base} WHERE link = ?", (link,))
+    is_here = cur.fetchone()[0]
+    if is_here == 0:
+        cur.execute(f"INSERT INTO {data_base}  VALUES (?, ?, ?)", (link, type, info))  # Добавление данных  в таблицу
+        con.commit()
+        return "New line added"
+        # выполняеся если ссылки нема в базе
+    else:
+        return "Link is already in bd"
+    # выполняется если ссылка есть в базе
+
+
+s = add_link_to_bd(link="Igor")
+print(s)
