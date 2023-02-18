56401a46e33a72f746085a46961bdf11b7547d4f
Igor <82223725+Igggor@users.noreply.github.com>
2023-02-18 22:39:10 +0300

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-from words_count import *
-from  Neron_for_case import get_state
-link = input("Введите ссылку\n")
-a = get_meta(link=link)
-a = counts_info_words(a)
-if (a != "Can't connect"):
-    print(get_state(a))
-else: print("Ошибка, невозможно подключиться к сайту")
